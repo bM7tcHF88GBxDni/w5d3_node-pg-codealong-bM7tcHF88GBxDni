6894e0ad03be1f93398f757cfca2188972ee6355
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2021-12-16 18:46:49 +0000

    Create .env, add .env to .gitignore, setup config.js and use config.js in db/index.js to connect to Heroku DB server

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 import  pg  from 'pg'; //import postgres
+import { dbCredentials } from "../config.js";
 
 const pool = new pg.Pool({
-    user: 'gcwbjfjnnmexop',
-    host: 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com',
-    database: 'd7sspfnokg2cv1',
-    password: '4d94162a96088569aa64859cd889ea76819588460010b35d5ad070c01786e567',
-    port: 5432,
+    user: dbCredentials.user,
+    host: dbCredentials.host,
+    database: dbCredentials.database,
+    password: dbCredentials.password,
+    port: dbCredentials.port,
     ssl: {rejectUnauthorized: false }
 });
 
